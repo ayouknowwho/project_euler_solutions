21e4ec8398fc40bbfb49c29fa919569ff3684303
ayouknowwho <64526596+ayouknowwho@users.noreply.github.com>
2023-03-22 09:25:48 +0000

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -12,7 +12,7 @@
 #
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
 # find the sum of the even-valued terms.
-
+#
 # (outputs the solution 4613732)
 
 a = 1
