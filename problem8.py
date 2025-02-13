36778c1c9f7f395665e4f13073e14655d7b794ac
ayouknowwho <64526596+ayouknowwho@users.noreply.github.com>
2025-02-13 11:06:21 +0000

    Update problem8.py

--- problem8.py
+++ problem8.py
@@ -1,4 +1,4 @@
-# Copyright 2023 by ayouknowwho
+# Copyright 2025 by ayouknowwho
 
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, 
 # either version 3 of the License, or (at your option) any later version.
